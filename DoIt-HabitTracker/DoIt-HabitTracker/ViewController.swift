989f29a8a1ae374cbb1e1dd0262d380d49bf83b2
Laura Bejan <laurabejan10@gmail.com>
2021-07-08 17:17:22 +0300

    Initial commit

---
+++ DoIt-HabitTracker/DoIt-HabitTracker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DoIt-HabitTracker
+//
+//  Created by Laura Bejan on 08.07.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
